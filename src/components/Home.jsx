ab231158aef311acc862f6e97e5a2f5eb112cbf5
K-S11145T <tanushsingh843@gmail.com>
2024-09-01 08:36:06 +0530

    change in code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,9 +25,9 @@ const Home = () => {
     <div className='p-10'>
       <Topnav />
 
-      <div className='w-full relative h-[80vh] flex-col items-center  flex  mt-12 '>
+      <div className='w-full relative h-[80vh] flex-col items-center  flex  mt-10 '>
 
-        <div id='img' className='h-[40vh] rounded-full shadow-lg shadow-black absolute top-[0] translate-y-[-50%] z-[2] overflow-hidden w-[40vh]'>
+        <div id='img' className='h-[35vh] rounded-full shadow-lg shadow-black absolute top-[0] translate-y-[-50%] z-[2] overflow-hidden w-[35vh]'>
           <img className='h-full w-full object-cover' src="https://w0.peakpx.com/wallpaper/863/441/HD-wallpaper-kyojuro-rengoku.jpg" alt="" />
         </div>
         <div id='div' className='w-[90%] group overflow-hidden shadow-2xl shadow-black flex relative justify-center items-end text-[23vh] h-[55vh] rounded-[100px] bg-zinc-900'>
@@ -36,11 +36,11 @@ const Home = () => {
           </div>
           <div className='leading-[20vh]' >
 
-            <h1 className='select-none text-zinc-500  group-hover:text-white duration-500 font-semibold'>TANUSH SINGH</h1>
+            <h1 className='select-none text-zinc-500 group-hover:text-white duration-500 font-semibold'>TANUSH SINGH</h1>
             <h1 className='text-xl text-zinc-400  group-hover:text-white duration-500 text-center py-5 font-black' >Web Designer , Frontend Developer , Video Editor</h1>
           </div>
         </div>
-        <button id='btn' className='py-3 px-4 border-2 text-sky-200 flex items-center justify-center gap-2 shadow-zinc-700 shadow-2xl  hover:shadow-white hover:animate-pulse hover:shadow-md hover:text-zinc-200  rounded-full mt-10'> <Link className='text-xl' to={"TANUSH SINGH.pdf"} download> Download CV </Link> <i class="ri-arrow-right-s-line text-3xl"></i></button>
+        <button id='btn' className='py-2 px-3 border-2 text-sky-200 flex items-center justify-center gap-2 shadow-zinc-700 shadow-2xl  hover:shadow-white hover:animate-pulse hover:shadow-md hover:text-zinc-200  rounded-full mt-7 '> <Link className='text-base' to={"TANUSH SINGH.pdf"} download> Download CV </Link> <i class="ri-arrow-right-s-line text-3xl"></i></button>
 
 
       </div>

baef03c806977d16752d270f8fbfb29078510b62
K-S11145T <tanushsingh843@gmail.com>
2025-02-25 14:35:06 +0530

    cv updated

--- src/components/Partials/Scene.jsx
+++ src/components/Partials/Scene.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react'
-import {  useTexture } from '@react-three/drei'
+import { OrbitControls, useTexture } from '@react-three/drei'
 import * as THREE from "three"
 import { useFrame } from '@react-three/fiber'
 
@@ -7,17 +7,24 @@ import { useFrame } from '@react-three/fiber'
 const Scene = () => {
   let tex = useTexture("./img.png")
   let cyl = useRef(null)
-  useFrame((state,delta)=>{
+  useFrame((state, delta) => {
     cyl.current.rotation.y += delta
+
   })
   return (
-    <group rotation={[0,1.4 ,0.3]}>
+    <>
+    
+    <OrbitControls enableZoom={false} />
+    <group rotation={[0, 1.4, 0.3]}>
+      
+      
 
-    <mesh ref={cyl} >
-      <cylinderGeometry args={[2.5, 2.5, 2, 60, 60, true]} />
-      <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide} />
-    </mesh>
+      <mesh ref={cyl} >
+        <cylinderGeometry args={[2.5, 2.5, 2, 60, 60, true]} />
+        <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide} />
+      </mesh>
     </group>
+    </>
   )
 }
 
